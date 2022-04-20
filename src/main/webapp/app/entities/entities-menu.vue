d373f9b64096d2fe1468e222b84bc0fbbef6ab79
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-04-20 00:21:39 +0000

    first commit

--- src/main/webapp/app/entities/entities-menu.vue
+++ src/main/webapp/app/entities/entities-menu.vue
@@ -1,5 +1,53 @@
 <template>
   <div>
+    <b-dropdown-item to="/medecin">
+      <font-awesome-icon icon="asterisk" />
+      <span>Medecin</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/secretaire">
+      <font-awesome-icon icon="asterisk" />
+      <span>Secretaire</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/patient">
+      <font-awesome-icon icon="asterisk" />
+      <span>Patient</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/detection">
+      <font-awesome-icon icon="asterisk" />
+      <span>Detection</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/rendez-vous">
+      <font-awesome-icon icon="asterisk" />
+      <span>Rendez Vous</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/visite">
+      <font-awesome-icon icon="asterisk" />
+      <span>Visite</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/compte">
+      <font-awesome-icon icon="asterisk" />
+      <span>Compte</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/maladie">
+      <font-awesome-icon icon="asterisk" />
+      <span>Maladie</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/classification">
+      <font-awesome-icon icon="asterisk" />
+      <span>Classification</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/image">
+      <font-awesome-icon icon="asterisk" />
+      <span>Image</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/unclassified">
+      <font-awesome-icon icon="asterisk" />
+      <span>Unclassified</span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/stade">
+      <font-awesome-icon icon="asterisk" />
+      <span>Stade</span>
+    </b-dropdown-item>
     <!-- jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here -->
   </div>
 </template>
