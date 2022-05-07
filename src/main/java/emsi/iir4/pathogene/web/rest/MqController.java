6f6131d4a4905840ccc66350e8dd0a175e15293c
KinoThe-Kafkaesque <abdellahkadem5@gmail.com>
2022-05-07 14:29:44 +0000

    mq

---
+++ src/main/java/emsi/iir4/pathogene/web/rest/MqController.java
@@ -0,0 +1,25 @@
+package emsi.iir4.pathogene.web.rest;
+
+import java.util.concurrent.CountDownLatch;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mq")
+public class MqController {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    @GetMapping("/recieve")
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
