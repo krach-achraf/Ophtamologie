8232a0c056a1b11872041ec57ac551d2f23d390b
krach-achraf <krachachraf98@gmail.com>
2022-05-10 18:27:06 +0000

    create image from stade

--- src/main/webapp/app/entities/maladie/maladie-details.component.ts
+++ src/main/webapp/app/entities/maladie/maladie-details.component.ts
@@ -1,15 +1,31 @@
 import { Component, Vue, Inject } from 'vue-property-decorator';
-
+import JhiDataUtils from '@/shared/data/data-utils.service';
 import { IMaladie } from '@/shared/model/maladie.model';
 import MaladieService from './maladie.service';
 import AlertService from '@/shared/alert/alert.service';
+import {IStade} from "@/shared/model/stade.model";
+import {IImage, Image} from "@/shared/model/image.model";
+import {mixins} from "vue-class-component";
+import ImageService from "@/entities/image/image.service";
 
-@Component
-export default class MaladieDetails extends Vue {
+const validations: any = {
+  image: {
+    photo: {},
+  },
+};
+
+@Component({
+  validations,
+})
+export default class MaladieDetails extends mixins(JhiDataUtils)  {
   @Inject('maladieService') private maladieService: () => MaladieService;
   @Inject('alertService') private alertService: () => AlertService;
+  @Inject('imageService') private imageService: () => ImageService;
 
-  public maladie: IMaladie = {};
+  private maladie: IMaladie = {};
+  private stades: IStade[] = [];
+  private stade: IStade;
+  private image: IImage = new Image();
 
   beforeRouteEnter(to, from, next) {
     next(vm => {
@@ -24,12 +40,51 @@ export default class MaladieDetails extends Vue {
       .find(maladieId)
       .then(res => {
         this.maladie = res;
+        this.stades = this.maladie.stades;
       })
       .catch(error => {
         this.alertService().showHttpError(this, error.response);
       });
   }
 
+  public prepareCeate(instance: IStade){
+    this.stade = instance;
+    if (<any>this.$refs.createEntity) {
+      (<any>this.$refs.createEntity).show();
+    }
+  }
+
+  public async saveImage(){
+    this.image.stade = this.stade;
+    try{
+      await this.imageService().create(this.image);
+      (<any>this.$refs.createEntity).hide();
+      return this.$root.$bvToast.toast('An image is created', {
+        toaster: 'b-toaster-top-center',
+        title: 'Info',
+        variant: 'info',
+        solid: true,
+        autoHideDelay: 5000,
+      });
+    }catch (e) {
+      console.log(e);
+    }
+
+  }
+
+  public clearInputImage(field, fieldContentType, idInput): void {
+    if (this.image && field && fieldContentType) {
+      if (Object.prototype.hasOwnProperty.call(this.image, field)) {
+        this.image[field] = null;
+      }
+      if (Object.prototype.hasOwnProperty.call(this.image, fieldContentType)) {
+        this.image[fieldContentType] = null;
+      }
+      if (idInput) {
+        (<any>this).$refs[idInput] = null;
+      }
+    }
+  }
   public previousState() {
     this.$router.go(-1);
   }
